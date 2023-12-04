f1c85b75a7a4f6bbde116fa3e1661115e45b7d6d
Med Jamel Bouassida <medjamel97@gmail.com>
2023-12-04 17:33:25 +0100

    Improved menu

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine;
 using UnityEngine.UI;
 
 public class SceneManagement : MonoBehaviour
